38cadc87f02511f3d6338e04820117a5af0d1419
Roberto Perdomo <tito.007@hotmail.es>
2020-09-22 20:04:41 -0600

    Creacion de Laboratorio 1 evaluacion 2, Materia Electiva 1

---
+++ app/src/main/java/com/example/ejercicio1evaluacion2/Model/ModelQuestion.java
@@ -0,0 +1,71 @@
+package com.example.ejercicio1evaluacion2.Model;
+
+
+public class ModelQuestion {
+    private int Idquestion;
+    private int Numberquestion;
+    private String Namequestion;
+    private String AnswerText1;
+    private String AnswerText2;
+    private String AnswerText3;
+    private int AnswerIdValue;
+
+
+    public int getAnswerIdValue() {
+        return AnswerIdValue;
+    }
+
+    public void setAnswerIdValue(int answerIdValue) {
+        AnswerIdValue = answerIdValue;
+    }
+
+    public int getIdquestion() {
+        return Idquestion;
+    }
+
+    public void setIdquestion(int idquestion) {
+        Idquestion = idquestion;
+    }
+
+    public int getNumberquestion() {
+        return Numberquestion;
+    }
+
+    public void setNumberquestion(int numberquestion) {
+        Numberquestion = numberquestion;
+    }
+
+    public String getNamequestion() {
+        return Namequestion;
+    }
+
+    public void setNamequestion(String namequestion) {
+        Namequestion = namequestion;
+    }
+
+    public String getAnswerText1() {
+        return AnswerText1;
+    }
+
+    public void setAnswerText1(String answerText1) {
+        AnswerText1 = answerText1;
+    }
+
+    public String getAnswerText2() {
+        return AnswerText2;
+    }
+
+    public void setAnswerText2(String answerText2) {
+        AnswerText2 = answerText2;
+    }
+
+    public String getAnswerText3() {
+        return AnswerText3;
+    }
+
+    public void setAnswerText3(String answerText3) {
+        AnswerText3 = answerText3;
+    }
+
+}
+
---
+++ app/src/main/java/com/example/ejercicio1evaluacion2/question1.java
@@ -0,0 +1,179 @@
+package com.example.ejercicio1evaluacion2;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.*;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.ejercicio1evaluacion2.Model.ModelQuestion;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class question1 extends AppCompatActivity {
+
+    private List<ModelQuestion> ListQuestions = new ArrayList<ModelQuestion>();
+
+    // creando objetos
+    RadioGroup rdbGroup1;
+    RadioButton rdtQuestion1, rdtQuestion2, rdtQuestion3,rdtAnswerd;
+    TextView NameQuestion, NumQuestion,CountView,SumPoint;
+    Button Nextquestion, OutApp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question1);
+
+        // llenamos la lista statica;
+        AsignarList();
+
+        //asignacion de objetos de la vista
+        rdbGroup1 = (RadioGroup) findViewById(R.id.rdbGroup1);
+        rdtQuestion1 = (RadioButton) findViewById(R.id.rdbOpcion1);
+        rdtQuestion2 = (RadioButton) findViewById(R.id.rdbOpcion2);
+        rdtQuestion3 = (RadioButton) findViewById(R.id.rdbOpcion3);
+
+        NameQuestion = (TextView) findViewById(R.id.QuestionName);
+        NumQuestion = (TextView) findViewById(R.id.NumQuestion);
+        CountView = (TextView) findViewById(R.id.Count);
+        SumPoint = (TextView) findViewById(R.id.Points);
+
+        Nextquestion = (Button) findViewById(R.id.btnNext);
+        OutApp = (Button) findViewById(R.id.btnOut);
+
+        AsignarPregunta(0);
+
+        Nextquestion.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                SumarPuntos();
+            }
+        });
+
+        OutApp.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                System.exit(0);
+            }
+        });
+
+    }
+
+    private void AsignarPregunta(int PreguntaActual)
+    {
+        String Estado = "Aprobado";
+        int NotaMinima = 6;
+        int ActualPoint = Integer.parseInt(SumPoint.getText().toString());
+        int converPregunt = PreguntaActual;
+        int count = converPregunt + 1;
+        int TotalPreguntas = 5;
+
+        if (count <= TotalPreguntas)
+        {
+            NameQuestion.setText(ListQuestions.get(converPregunt).getNamequestion());
+            NumQuestion.setText("Pregunta # "+ ListQuestions.get(converPregunt).getNumberquestion());
+            rdtQuestion1.setText(ListQuestions.get(converPregunt).getAnswerText1());
+            rdtQuestion2.setText(ListQuestions.get(converPregunt).getAnswerText2());
+            rdtQuestion3.setText(ListQuestions.get(converPregunt).getAnswerText3());
+            CountView.setText(Integer.toString(count));
+        }
+    else
+        {
+            NumQuestion.setText("Nota Obtenida: " + ActualPoint +".00");
+
+            if (ActualPoint < NotaMinima)
+            {
+                Estado = "Reprobado";
+            }
+            NameQuestion.setText("Estado: " + Estado);
+
+            Nextquestion.setVisibility(View.INVISIBLE);
+            rdtQuestion1.setVisibility(View.INVISIBLE);
+            rdtQuestion2.setVisibility(View.INVISIBLE);
+            rdtQuestion3.setVisibility(View.INVISIBLE);
+        }
+
+    }
+
+    private void SumarPuntos()
+    {
+        int valorCorrecto = 2;
+        int SinEspecificar = -1;
+        int RespuestaPreguntaActual;
+        rdtAnswerd = (RadioButton) findViewById(rdbGroup1.getCheckedRadioButtonId());
+        int Respuesta = rdbGroup1.indexOfChild(rdtAnswerd);
+        int ActualPoint = Integer.parseInt(SumPoint.getText().toString());
+        int PreguntaActual = Integer.parseInt(CountView.getText().toString());
+
+        if (Respuesta != SinEspecificar){
+            int calcularposicionenlista = PreguntaActual -1 ;
+            RespuestaPreguntaActual = ListQuestions.get(calcularposicionenlista).getAnswerIdValue();
+
+            if (Respuesta == RespuestaPreguntaActual)
+            {
+                ActualPoint += valorCorrecto;
+                SumPoint.setText(Integer.toString(ActualPoint));
+            }
+            rdtAnswerd.setChecked(false);
+        }else{
+            Toast.makeText(this, "Por favor seleccione una respuesta", Toast.LENGTH_LONG).show();
+        }
+
+        AsignarPregunta(PreguntaActual);
+    }
+
+
+    private void AsignarList()
+    {
+        ModelQuestion Pregunta1 = new ModelQuestion();
+            Pregunta1.setIdquestion(0);
+            Pregunta1.setNumberquestion(1);
+            Pregunta1.setNamequestion("¿setText es para?");
+            Pregunta1.setAnswerText1("Borrar Texto");
+            Pregunta1.setAnswerText2("Asignar Texto");
+            Pregunta1.setAnswerText3("Ocultar texto");
+            Pregunta1.setAnswerIdValue(1);
+        ListQuestions.add(Pregunta1);
+
+        ModelQuestion Pregunta2 = new ModelQuestion();
+            Pregunta2.setIdquestion(1);
+            Pregunta2.setNumberquestion(2);
+            Pregunta2.setNamequestion("¿getText es para?");
+            Pregunta2.setAnswerText1("Borrar Texto");
+            Pregunta2.setAnswerText2("Asignar Texto");
+            Pregunta2.setAnswerText3("Recuperar texto");
+            Pregunta2.setAnswerIdValue(2);
+        ListQuestions.add(Pregunta2);
+
+        ModelQuestion Pregunta3 = new ModelQuestion();
+            Pregunta3.setIdquestion(2);
+            Pregunta3.setNumberquestion(3);
+            Pregunta3.setNamequestion("¿Toast es para?");
+            Pregunta3.setAnswerText1("Cerrar la app");
+            Pregunta3.setAnswerText2("Mostrar mensajes");
+            Pregunta3.setAnswerText3("Sumar numeros");
+            Pregunta3.setAnswerIdValue(1);
+        ListQuestions.add(Pregunta3);
+
+        ModelQuestion Pregunta4 = new ModelQuestion();
+            Pregunta4.setIdquestion(3);
+            Pregunta4.setNumberquestion(4);
+            Pregunta4.setNamequestion("¿Finish es para?");
+            Pregunta4.setAnswerText1("Borrar Texto");
+            Pregunta4.setAnswerText2("Asignar Texto");
+            Pregunta4.setAnswerText3("Cerrar Actividad");
+            Pregunta4.setAnswerIdValue(2);
+        ListQuestions.add(Pregunta4);
+
+        ModelQuestion Pregunta5 = new ModelQuestion();
+            Pregunta5.setIdquestion(4);
+            Pregunta5.setNumberquestion(5);
+            Pregunta5.setNamequestion("¿TextView es para?");
+            Pregunta5.setAnswerText1("Mostrar Texto");
+            Pregunta5.setAnswerText2("Multiplicar");
+            Pregunta5.setAnswerText3("Dividir");
+            Pregunta5.setAnswerIdValue(0);
+        ListQuestions.add(Pregunta5);
+    }
+
+}
